8c52862ffee4e5897a73bfe75fc3bec51321de93
MSP Markus <m.platter@die-fabrik.net>
2023-10-01 11:56:27 +0200

    Initial Commit

---
+++ WeatherBot/WeatherBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBotApp.swift
+//  WeatherBot
+//
+//  Created by Markus Platter on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
